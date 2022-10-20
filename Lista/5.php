772393df7705959247e36535a0c84952fb975620
Lucas Bechtlufft <111580061+Lucas-Bechtlufft@users.noreply.github.com>
2022-10-20 19:43:43 -0300

    Adicionando Informações

---
+++ Lista/5.php
@@ -0,0 +1,8 @@
+<?php
+include_once "5A.php";
+$html = file_get_contents("5.html");
+$html = str_replace("{title}", "Array Aleatório", $html);
+$html = str_replace("{array}", print_r(criarArrayAleatorio(10), true), $html);
+echo $html;
+
+?>
